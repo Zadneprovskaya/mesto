b3f5f6105c82e6bb425620494552251ca3284465
Irina Zadneprovskaya <iszadneprovskaya@pharmstd.ru>
2022-12-03 00:38:51 +0300

    Mesto:Finished project-3

--- scripts/script.js
+++ scripts/script.js
@@ -5,37 +5,35 @@ let profileName = profileInfo.querySelector('.profile-info__name');
 let profileJob = profileInfo.querySelector('.profile-info__description');
 let formElement = container.querySelector('.popup');
 let formContainer = formElement.querySelector('.popup__container');
+let formProfile = formContainer.querySelector('.popup__form-profile');
 let closeButton = formElement.querySelector('.popup__button-close');
 let nameInput = formElement.querySelector('.popup__text_value_name');
 let jobInput = formElement.querySelector('.popup__text_value_description');
-let valueName = profileName.textContent;
-let valueJob = profileJob.textContent;
+let saveButton = formElement.querySelector('.popup__submit-btn');
+let formPopup = document.forms.popupForm;
 
 
 
 let openForm = () => {
-  if (formElement.classList.contains('popup_opened')) {
-      formElement.classList.remove('popup_opened');
-      nameInput.setAttribute('value',valueName);
-      jobInput.setAttribute('value',valueJob);
+  if (!formElement.classList.contains('popup_opened')) {
+      formPopup.newName.value = profileName.textContent;
+      formPopup.newDescription.value = profileJob.textContent;
+      formElement.classList.add('popup_opened');
   }
 }
 
 let closeForm = (event) => {
-  if(!formContainer.contains(event.target) || event.target === closeButton) {
-    event.preventDefault();
-    formElement.classList.add('popup_opened');
-  }
+    formElement.classList.remove('popup_opened');
 }
 
 let formSubmitHandler = (event) => {
   event.preventDefault();
-  profileName.textContent = document.forms.popupForm.newName.value;
-  profileJob.textContent = document.forms.popupForm.newDescription.value;
-  formElement.classList.add('popup_opened');
+  profileName.textContent = formPopup.newName.value;
+  profileJob.textContent = formPopup.newDescription.value;
+  closeForm(event);
 }
 
-  formContainer.addEventListener('submit', formSubmitHandler);
+  formProfile.addEventListener('submit', formSubmitHandler);
   editButton.addEventListener ('click',openForm);
-  formElement.addEventListener ('click',closeForm);
+  closeButton.addEventListener ('click',closeForm);
   
