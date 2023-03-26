583da6cf3553d2835091f27e83fa016f3ed7b82b
Irina Zadneprovskaya <irinosik1182@gmail.com>
2023-03-27 01:54:39 +0300

    Finish 8_2

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    main: './src/pages/index.js',
-    devtool: 'source-map',
+    main: './src/pages/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
